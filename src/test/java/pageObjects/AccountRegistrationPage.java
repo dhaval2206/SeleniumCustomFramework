a571e775df85c09fa3ee609ddc4ec9e614cd44e4
dhaval2206 <43372618+dhaval2206@users.noreply.github.com>
2025-04-01 18:35:17 +0530

    First commit

---
+++ src/test/java/pageObjects/AccountRegistrationPage.java
@@ -0,0 +1,75 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountRegistrationPage extends BasePage {
+
+	public AccountRegistrationPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "//input[@id='input-firstname']")
+	WebElement txtFirstName;
+
+	@FindBy(xpath = "//input[@id='input-lastname']")
+	WebElement txtLastName;
+
+	@FindBy(xpath = "//input[@id='input-email']")
+	WebElement txtEmail;
+
+	@FindBy(xpath = "//input[@id='input-telephone']")
+	WebElement txtTelephone;
+
+	@FindBy(xpath = "//input[@id='input-password']")
+	WebElement txtPassword;
+
+	@FindBy(xpath = "//input[@id='input-confirm']")
+	WebElement txtCnfPassword;
+
+	@FindBy(xpath = "//input[@name='agree']")
+	WebElement chkdPolicy;
+
+	@FindBy(xpath = "//input[@value='Continue']")
+	WebElement btnContinue;
+
+	@FindBy(xpath = "//h1[normalize-space()='Your Account Has Been Created!']")
+	WebElement msgConfirmation;
+
+	public void setFirstName(String fName) {
+		txtFirstName.sendKeys(fName);
+	}
+
+	public void setLastName(String lName) {
+		txtLastName.sendKeys(lName);
+	}
+
+	public void setEmail(String email) {
+		txtEmail.sendKeys(email);
+	}
+
+	public void setTelephone(String telephone) {
+		txtTelephone.sendKeys(telephone);
+	}
+
+	public void setPassword(String pwd) {
+		txtFirstName.sendKeys(pwd);
+	}
+
+	public void setPrivacyPolicy() {
+		chkdPolicy.click();
+	}
+
+	public void clickContinueButton() {
+		btnContinue.click();
+	}
+
+	public String getConfirmationmsg(String pwd) {
+		try {
+			return msgConfirmation.getText();
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
+}
